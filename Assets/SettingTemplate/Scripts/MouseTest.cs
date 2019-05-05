eed5f794fe563164b9d7bd71df6f281d8d448e48
0kk470 <ookk47oo@gmail.com>
2019-05-05 21:29:15 +0800

    Add MouseEnter MouseExit Test

---
+++ Assets/SettingTemplate/Scripts/MouseTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseTest : MonoBehaviour {
+
+    private void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+        Debug.Log("Enter");
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Leave");
+    }
+}
